5c88e4606b05a219a0469dad49393dc7b7f6b8ea
AIguy-x <129828366+AIguy-x@users.noreply.github.com>
2023-12-29 15:47:07 -0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,11 +5,11 @@ const strainsCleanedPath = 'https://aiguy-x.github.io/thc-calculator/strains_cle
 fetch(strainsCleanedPath)
   .then(response => response.json())
   .then(strains => {
-    const strainSelect = document.getElementById('strainSelect'); // Ensure this is the correct ID for your select element
+    const strainSelect = document.getElementById('strainSelect');
     strains.forEach(strain => {
       const option = document.createElement('option');
-      option.value = strain.Name; // Use 'Name' from your 'strains_cleaned.json'
-      option.textContent = strain.Name; // Use 'Name' for the dropdown text as well
+      option.value = strain.Name;
+      option.textContent = strain.Name;
       strainSelect.appendChild(option);
     });
   })
@@ -18,18 +18,17 @@ fetch(strainsCleanedPath)
 // Event listener for the strain selection change
 document.getElementById('strainSelect').addEventListener('change', function(event) {
   const selectedStrainName = event.target.value;
-  const thcInput = document.getElementById('thcInput'); // Make sure this is the correct ID for your THC input
-  const cbdInput = document.getElementById('cbdInput'); // Make sure this is the correct ID for your CBD input
-  
-  // Find the selected strain data
+  const thcInput = document.getElementById('thcInput');
+  const cbdInput = document.getElementById('cbdInput');
+
+  // Fetch again or use cached data
   fetch(strainsCleanedPath)
     .then(response => response.json())
     .then(strains => {
       const selectedStrain = strains.find(strain => strain.Name === selectedStrainName);
       if (selectedStrain) {
-        // Update THC and CBD percentages
-        thcInput.value = selectedStrain['THC%'].match(/\d+/)[0]; // Extracts the number from strings like 'THC 23%'
-        cbdInput.value = selectedStrain['Other_Cannabinoids'].match(/\d+/)[0] || '0'; // Extracts the number, defaults to '0' if not found
+        thcInput.value = selectedStrain['THC%'].match(/\d+/) ? selectedStrain['THC%'].match(/\d+/)[0] : 'N/A';
+        cbdInput.value = selectedStrain['Other_Cannabinoids'].match(/\d+/) ? selectedStrain['Other_Cannabinoids'].match(/\d+/)[0] : 'N/A';
       }
     })
     .catch(error => console.error('Error finding selected strain data:', error));
