f5bea49b031d0667df29e796f8d8eed759d780ae
AIguy-x <129828366+AIguy-x@users.noreply.github.com>
2023-12-29 21:13:12 -0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -8,8 +8,8 @@ fetch(strainsCleanedPath)
     const strainSelect = document.getElementById('strainSelect');
     strains.forEach(strain => {
       const option = document.createElement('option');
-      option.value = strain.Name;
-      option.textContent = strain.Name;
+      option.value = strain.Name; // Assuming 'Name' is the key for the strain name
+      option.textContent = strain.Name; // The text shown in the dropdown
       strainSelect.appendChild(option);
     });
   })
@@ -18,8 +18,8 @@ fetch(strainsCleanedPath)
 // Event listener for the strain selection change
 document.getElementById('strainSelect').addEventListener('change', function(event) {
   const selectedStrainName = event.target.value;
-  const thcInput = document.getElementById('thcInput');
-  const cbdInput = document.getElementById('cbdInput');
+  const thcInput = document.getElementById('thcPercentage');
+  const cbdInput = document.getElementById('cbdPercentage');
 
   // Fetch again or use cached data
   fetch(strainsCleanedPath)
@@ -27,9 +27,26 @@ document.getElementById('strainSelect').addEventListener('change', function(even
     .then(strains => {
       const selectedStrain = strains.find(strain => strain.Name === selectedStrainName);
       if (selectedStrain) {
-        thcInput.value = selectedStrain['THC%'].match(/\d+/) ? selectedStrain['THC%'].match(/\d+/)[0] : 'N/A';
-        cbdInput.value = selectedStrain['Other_Cannabinoids'].match(/\d+/) ? selectedStrain['Other_Cannabinoids'].match(/\d+/)[0] : 'N/A';
+        // Update THC and CBD percentages
+        thcInput.value = selectedStrain['THC%'].match(/\d+/) ? selectedStrain['THC%'].match(/\d+/)[0] + '%' : 'N/A';
+        cbdInput.value = selectedStrain['Other_Cannabinoids'].match(/\d+/) ? selectedStrain['Other_Cannabinoids'].match(/\d+/)[0] + '%' : 'N/A';
       }
     })
     .catch(error => console.error('Error finding selected strain data:', error));
 });
+
+// Event listener for the calculator form submission
+document.getElementById('calculator').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Here, you would call the function that performs the calculation based on user inputs
+  // Example: calculateDosage();
+});
+
+// Example function for calculation (to be implemented)
+function calculateDosage() {
+  // Your calculation logic here
+  // Example: Update the result element with the calculated dosage
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = 'Calculated Dosage: ...'; // Replace with actual result
+  // You'll need to retrieve the values from the input fields and perform your calculation logic
+}
